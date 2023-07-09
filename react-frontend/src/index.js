89c9ee4eae93800c8a1ceea16f98067b31a578c3
onions41 <kblee89@outlook.com>
2023-07-09 15:55:07 -0700

    Redux provider added

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider as ReduxProvider } from "react-redux";
 import CssBaseline from "@mui/material/CssBaseline";
 
 import App from "./App";
+import store from "./store/store"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <CssBaseline />
-    <App />
+    <ReduxProvider store={store}>
+      <CssBaseline />
+      <App />
+    </ReduxProvider>
   </>
 );
