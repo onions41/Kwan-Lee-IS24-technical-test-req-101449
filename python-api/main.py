878eff4e5769e91ae810cec671a9b45e49b80cfa
onions41 <kblee89@outlook.com>
2023-07-10 16:37:48 -0700

    Changed API port to 8000

--- python-api/main.py
+++ python-api/main.py
@@ -126,10 +126,11 @@ def delete_product(id):
 
 if __name__ == "__main__":
     # API is started this way only during development
+    # When running with Gunicorn production server:
     # Flask only listens at 127.0.0.1 by default, but
     # it needs to listen at all addresses when running in a container, because
     # Docker just chooses a random container address to forward requests
-    app.run(debug=True)
+    app.run(port=8000, debug=True)
     # app.run(host="0.0.0.0")
     # Gunicorn is used instead for deployment in the Docker container
     
