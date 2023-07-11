12656bedc7ff547efc3a4524adc06d59c541849f
onions41 <kblee89@outlook.com>
2023-07-10 21:39:42 -0700

    Improved comments

--- react-frontend/serve.js
+++ react-frontend/serve.js
@@ -1,18 +1,23 @@
-// Imports
+/**
+ * Very simple Express server for serving the built front-end application
+ */
+
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
-// This is to serve the static files in the build folder
+// Serves the assets in the build folder
 app.use(express.static(path.join(__dirname, "build")));
 
-// This is to serve the bundled react appp itself
+// Serves the bundled React app itself
+// Wildcard used to capture all routes, which is required
+// because the React app uses client-side routing
 app.get("/*", function (_req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-// Start the server
+// Starts the Express server
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
